b2b537767a629ea335ae2895347831b11602ce74
Isabel <isabel.martinez1@bsc.es>
2024-07-15 12:30:15 +0200

    changing : to =

--- src/io.jl
+++ src/io.jl
@@ -60,7 +60,7 @@ function create_default_npiparameters()
     npiparams_dict["ϕs"] = [1.0]
     npiparams_dict["δs"] = [0.0]
     npiparams_dict["tᶜs"] =  [1]
-    npiparams_dict["are_there_npi"]: true
+    npiparams_dict["are_there_npi"] = true
 
     return npiparams_dict
 end
