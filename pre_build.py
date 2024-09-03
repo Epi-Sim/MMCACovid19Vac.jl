f55303e24706eb89397ac6262ae93aa0e8b432cf
lknoxstr <lknoxstr@bsc.es>
2024-09-03 16:44:47 +0200

    pre-build compile step when importing the python interface

---
+++ pre_build.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+from pathlib import Path
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        print("CustomBuildHook initialized")
+        try:
+            print("Compiling EpiSim...")
+            subprocess.run(["bash", "install.sh"], check=True)
+            print("Compilation successful")
+        except subprocess.CalledProcessError as e:
+            print(f"Compilation failed: {e}", file=sys.stderr)
+            sys.exit(1)
+
+    def clean(self, versions):
+        print("CustomBuildHook clean method called")
+        with open("build_log.txt", "a") as f:
+            f.write("CustomBuildHook clean method called\n")
