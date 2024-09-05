f1282e3ee7581606e5163cb55eb9aea774ef3060
lknoxstr <lknoxstr@bsc.es>
2024-09-05 15:51:43 +0200

    enable the old engine, use different save functions

--- src/engine.jl
+++ src/engine.jl
@@ -1,7 +1,9 @@
 using NetCDF
 using NCDatasets
 using DataFrames
-using MMCACovid19Vac
+using MMCACovid19Vac: NPI_Params, init_NPI_parameters_struct, init_pop_param_struct, init_epi_parameters_struct
+import MMCACovid19Vac
+import MMCAcovid19
 
 include("io.jl")
 
@@ -48,7 +50,6 @@ function read_input_files(::AbstractEngine, config::Dict, data_path::String, ins
     data_dict       = config["data"]
     simulation_dict = config["simulation"]
     npi_params_dict = config["NPI"]
-    init_format      = get(simulation_dict, "init_format", "netcdf")
 
     #########################
     # Simulation output 
@@ -80,18 +81,12 @@ function read_input_files(::AbstractEngine, config::Dict, data_path::String, ins
     end
 
     # use initial compartments matrix to initialize simulations
-    if init_format == "netcdf"
-        @info "Reading initial conditions from: $(init_condition_path)"
-        initial_compartments = ncread(init_condition_path, "data")
-    elseif init_format == "hdf5"
-        # TODO: does this path work?
-        initial_compartments = h5open(init_condition_path, "r") do file
-            read(file, "data")
-        end
-    else
-        @error "init_format must be one of : netcdf/hdf5"
-        return 1
-    end
+    @info "Reading initial conditions from: $(init_condition_path)"
+    initial_compartments = NCDataset(init_condition_path)["data"]
+    @info "Initial compartments shape", dimsize(initial_compartments)
+
+    # check the order of the epi states for later
+    # NCDatasets(init_condition_path)["epi_states"][:]
 
     # Loading mobility network
     mobility_matrix_filename = joinpath(data_path, data_dict["mobility_matrix_filename"])
@@ -122,7 +117,19 @@ function run_engine_io(engine::AbstractEngine, config::Dict, data_path::String, 
 
     npi_params, network_df, metapop_df, initial_compartments = read_input_files(engine, config, data_path, instance_path, init_condition_path)
 
-    epi_params, population, coords = run_engine(engine, config, npi_params, network_df, metapop_df, initial_compartments)
+    if engine == MMCACovid19VacEngine()
+        @assert "V" in dimnames(initial_compartments)
+        epi_params, population, coords = run_engine(engine, config, npi_params, network_df, metapop_df, initial_compartments[:, :, :, :])
+    elseif engine == MMCACovid19Engine()
+        if "V" in dimnames(initial_compartments)
+            @warn "Vaccination dimension found in initial conditions but engine does not support it. Dropping vaccination dimension."
+            # sum across vaccination states
+            initial_compartments = dropdims(sum(initial_compartments, dims=3), dims=3)
+        end
+        epi_params, population, coords = run_engine(engine, config, npi_params, network_df, metapop_df, initial_compartments)
+    else
+        @error "Unsupported engine: $engine"
+    end
 
     @info "\t- Save full output = " save_full_output
     if time_step_tosave !== nothing
@@ -219,115 +226,53 @@ function run_engine(::MMCACovid19VacEngine, config::Dict, npi_params::NPI_Params
     ################ RUN THE SIMULATION ####################
     ########################################################
 
-    set_compartments!(epi_params, population, initial_compartments)
+    MMCACovid19Vac.set_compartments!(epi_params, population, initial_compartments)
 
-    run_epidemic_spreading_mmca!(epi_params, population, npi_params, tᵛs, ϵᵍs; verbose = true )
+    MMCACovid19Vac.run_epidemic_spreading_mmca!(epi_params, population, npi_params, tᵛs, ϵᵍs; verbose = true )
 
     return epi_params, population, Dict(:T_coords => T_coords, :G_coords => G_coords, :M_coords => M_coords)
 end
 
 
-function run_engine(::MMCACovid19Engine, config::Dict, data_path::String, instance_path::String, init_condition_path::String)
-
-    ###########################################
-    ############# FILE READING ################
-    ###########################################
-    
+# one less compartment in the input state because we don't consider vaccination state
+function run_engine(::MMCACovid19Engine, config::Dict, npi_params::NPI_Params, network_df::DataFrame, metapop_df::DataFrame, initial_compartments::Array{Float64, 3})
+    @info "Running MMCACovid19Engine"
     simulation_dict = config["simulation"]
-    data_dict       = config["data"]
     epi_params_dict = config["epidemic_params"]
     pop_params_dict = config["population_params"]
-    npi_params_dict = config["NPI"]
-
-    #########################
-    # Simulation output 
-    #########################
-    output_path = joinpath(instance_path, "output")
-    if !isdir(output_path)
-        println("Creating output folder: $output_path")
-        mkpath(output_path)
-    end
-
-    output_format    = simulation_dict["output_format"]
-    save_full_output = get(simulation_dict, "save_full_output", false)
-    save_time_step   = get(simulation_dict, "save_time_step", nothing)
-    init_format      = get(simulation_dict, "init_format", "netcdf")
-
-    #####################
-    # Initial Condition
-    #####################
-
-    if !isfile(init_condition_path) || length(init_condition_path) == 0
-        init_condition_path = joinpath(data_path, get(data_dict, "initial_condition_filename", nothing))
-    end
-
-
-    # use initial compartments matrix to initialize simulations
-    if init_format == "netcdf"
-        @info "Reading initial conditions from: $(init_condition_path)"
-        initial_compartments = ncread(init_condition_path, "data")
-    elseif init_format == "hdf5"
-        initial_compartments = h5open(init_condition_path, "r") do file
-            read(file, "data")
-        end
-    else
-        @error "init_format must be one of : netcdf/hdf5"
-        return 1
-    end
-
 
     ########################################
     ####### VARIABLES INITIALIZATION #######
     ########################################
+    @info "Initializing variables"
 
     # Reading simulation start and end dates
     first_day = Date(simulation_dict["start_date"])
     last_day  = Date(simulation_dict["end_date"])
-    # Converting dates to time steps
     T = (last_day - first_day).value + 1
-    # Array with time coordinates (dates)
     T_coords  = string.(collect(first_day:last_day))
 
-    # Loading metapopulation patches info (surface, label, population by age)
-    metapop_data_filename = joinpath(data_path, data_dict["metapopulation_data_filename"])
-    metapop_df = CSV.read(metapop_data_filename, DataFrame, types=Dict(:id => String))
-
-    # Loading mobility network
-    mobility_matrix_filename = joinpath(data_path, data_dict["mobility_matrix_filename"])
-    network_df  = CSV.read(mobility_matrix_filename, DataFrame)
-
-    # Metapopulations patches coordinates (labels)
-    M_coords = map(String,metapop_df[:, "id"])
+    M_coords = map(String, metapop_df[:, "id"])
     M = length(M_coords)
 
-    # Coordinates for each age strata (labels)
     G_coords = map(String, pop_params_dict["G_labels"])
     G = length(G_coords)
 
     ####################################################
     #####   INITIALIZATION OF DATA Structures   ########
     ####################################################
+    @info "Initializing data structures"
 
     ## POPULATION PARAMETERS
-    # population = MMCACovid19Vac.init_pop_param_struct(G, M, G_coords, pop_params_dict, metapop_df, network_df)
-    #  crear estructura a a mano
+    population = init_pop_param_struct(G, M, G_coords, pop_params_dict, metapop_df, network_df)
+    # population params are identical between models but MMCACovid19Vac has a better init function
+    population = MMCAcovid19.Population_Params(population.G, population.M, population.nᵢᵍ, population.kᵍ, population.kᵍ_h, population.kᵍ_w, population.C, population.pᵍ, population.edgelist, population.Rᵢⱼ, population.sᵢ, population.ξ, population.σ)
+    
     ## EPIDEMIC PARAMETERS 
-    # epi_params = MMCACovid19Vac.init_epi_parameters_struct(G, M, T, G_coords, epi_params_dict)
-    #  crear estructura a a mano
+    epi_params = epi_params_struct_mmca_covid19(G, M, T, G_coords, epi_params_dict)
 
-    # @assert size(initial_compartments) == (G, M, epi_params.V, epi_params.NumComps)
-
-    #########################################################
-    # Containment measures
-    #########################################################
-
-    # Daily Mobility reduction
-    kappa0_filename = get(data_dict, "kappa0_filename", nothing)
-    npi_params = MMCACovid19Vac.init_NPI_parameters_struct(data_path, npi_params_dict, kappa0_filename, first_day)
-
-    # vac_parms = Vaccination_Params(tᵛs, ϵᵍs)
-
-    ##################################################
+    @info "Initial compartments shape", size(initial_compartments)
+    @assert size(initial_compartments) == (G, M, 10)
 
     @info "- Initializing MMCA epidemic simulations"
     @info "\t- first_day_simulation = "  first_day
@@ -335,18 +280,71 @@ function run_engine(::MMCACovid19Engine, config::Dict, data_path::String, instan
     @info "\t- G (agent class) = " G
     @info "\t- M (n. of metapopulations) = "  M
     @info "\t- T (simulation steps) = " T
-    @info "\t- N. of epi compartments = " epi_params.NumComps
-    @info "\t- Save full output = " save_full_output
-    if save_time_step !== nothing
-        @info "\t- Save time step at t=" save_time_step
-    end
+    @info "\t- N. of epi compartments = " 10
 
     ########################################################
     ################ RUN THE SIMULATION ####################
     ########################################################
 
-    MMCACovid19Vac.set_compartments!(epi_params, population, initial_compartments)
+    # Extract initial compartments
+    E₀ = initial_compartments[:, :, 2]
+    A₀ = initial_compartments[:, :, 3]
+    I₀ = initial_compartments[:, :, 4]
 
-    MMCACovid19Vac.run_epidemic_spreading_mmca!(epi_params, population, npi_params, tᵛs, ϵᵍs; verbose = true )
+    # Initialize the epidemic
+    MMCAcovid19.set_initial_infected!(epi_params, population, E₀, A₀, I₀)
 
+    # Extract containment strategy parameters
+    tᶜs = Int64.(get(epi_params_dict, "tᶜs", [])) # containment start days
+    κ₀s = Float64.(get(epi_params_dict, "κ₀s", [])) # mobility reduction factors
+    ϕs = Float64.(get(epi_params_dict, "ϕs", [])) # confined household permeability
+    δs = Float64.(get(epi_params_dict, "δs", [])) # social distancing
+
+    # Run the model with containment strategy
+    MMCAcovid19.run_epidemic_spreading_mmca!(epi_params, population, tᶜs, κ₀s, ϕs, δs; verbose = true)
+
+    return epi_params, population, Dict(:T_coords => T_coords, :G_coords => G_coords, :M_coords => M_coords)
+end
+
+function epi_params_struct_mmca_covid19(G::Int64, M::Int64, T::Int64,
+                                    G_coords::Array{String, 1}, 
+                                    epi_params_dict::Dict)
+
+    # Scaling of the asymptomatic infectivity
+    scale_β = epi_params_dict["scale_β"]
+    # Infectivity of Symptomatic
+    βᴵ = epi_params_dict["βᴵ"]
+    # Infectivity of Asymptomatic
+    if haskey(epi_params_dict, "βᴬ")
+        βᴬ = epi_params_dict["βᴬ"]
+    elseif haskey(epi_params_dict, "scale_β")
+        βᴬ = scale_β * βᴵ
+    else
+        @error "Either βᴬ or scale_β should be provided"
+    end
+    # Exposed rate
+    ηᵍ = Float64.(epi_params_dict["ηᵍ"])
+    # Asymptomatic rate
+    αᵍ = Float64.(epi_params_dict["αᵍ"])
+    # Infectious rate
+    μᵍ = Float64.(epi_params_dict["μᵍ"])
+
+    ## EPIDEMIC PARAMETERS TRANSITION RATES VACCINATION
+
+    # Direct death probability
+    θᵍ = Float64.(epi_params_dict["θᵍ"])
+    # Hospitalization probability
+    γᵍ = Float64.(epi_params_dict["γᵍ"])
+    # Fatality probability in ICU
+    ωᵍ = Float64.(epi_params_dict["ωᵍ"])
+    # Pre-deceased rate
+    ζᵍ = Float64.(epi_params_dict["ζᵍ"])
+    # Pre-hospitalized in ICU rate
+    λᵍ = Float64.(epi_params_dict["λᵍ"])
+    # Death rate in ICU
+    ψᵍ = Float64.(epi_params_dict["ψᵍ"])
+    # ICU discharge rate
+    χᵍ = Float64.(epi_params_dict["χᵍ"])
+
+    return MMCAcovid19.Epidemic_Params(βᴵ, βᴬ, ηᵍ, αᵍ, μᵍ, θᵍ, γᵍ, ζᵍ, λᵍ, ωᵍ, ψᵍ, χᵍ, G, M, T)
 end
--- src/io.jl
+++ src/io.jl
@@ -1,3 +1,6 @@
+import MMCAcovid19
+import MMCACovid19Vac
+
 function update_config!(config, cmd_line_args)
     # Define dictionary containing epidemic parameters
 
@@ -59,4 +62,120 @@ function save_time_step(epi_params, population, output_path::String, export_comp
     @info "\t- filename: $(filename)"
     save_simulation_hdf5(epi_params, population, filename; 
                         export_time_t = export_compartments_time_t)
+end
+
+
+"""
+save_simulation_netCDF(epi_params::MMCACovid19Vac.Epidemic_Params, 
+                                    population::MMCACovid19Vac.Population_Params,
+                                    output_fname::String;
+                                    G_coords= nothing,
+                                    M_coords = nothing,
+                                    T_coords = nothing
+                                    )
+
+    Save the full simulations.
+
+    # Arguments
+
+    - `epi_params::MMCACovid19Vac.Epidemic_Params`: Structure that contains all epidemic parameters
+    and the epidemic spreading information.
+    - `population::MMCACovid19Vac.Population_Params`: Structure that contains all the parameters
+    related with the population.
+    - `output_fname::String`: Output filename.
+
+    ## Optional
+    - `G_coords = nothing`: Array::{String} of size G containing the labels for age strata
+    - `M_coords = nothing`: Array::{String} of size M containing the labels for the patches
+    - `T_coords = nothing`: Array::{String} of size t containing the labels for the time (dates)
+    - `export_time_t = -1`: Time step to ve saved instead of the full simulation.
+"""
+function save_simulation_netCDF( epi_params::MMCACovid19Vac.Epidemic_Params, 
+                                 population::MMCACovid19Vac.Population_Params,
+                                 output_fname::String;
+                                 G_coords = nothing,
+                                 M_coords = nothing,
+                                 T_coords = nothing,
+                                 V_coords = nothing
+                                )
+    G = population.G
+    M = population.M
+    T = epi_params.T
+    V = epi_params.V
+    S = epi_params.NumComps
+    S_coords = epi_params.CompLabels
+    V_coords = epi_params.VaccLabels
+
+    if isnothing(G_coords)
+        G_coords = collect(1:G)
+    end
+    if isnothing(M_coords)
+        M_coords = collect(1:M)
+    end
+    if isnothing(T_coords)
+        T_coords = collect(1:T) 
+    end
+
+    compartments = zeros(Float64, G, M, T, V, S);
+
+    # Adding 
+    compartments[:, :, :, :, 1]  .= (epi_params.ρˢᵍᵥ + epi_params.CHᵢᵍᵥ) .* population.nᵢᵍ
+    compartments[:, :, :, :, 2]  .= epi_params.ρᴱᵍᵥ .* population.nᵢᵍ
+    compartments[:, :, :, :, 3]  .= epi_params.ρᴬᵍᵥ .* population.nᵢᵍ
+    compartments[:, :, :, :, 4]  .= epi_params.ρᴵᵍᵥ .* population.nᵢᵍ
+    compartments[:, :, :, :, 5]  .= epi_params.ρᴾᴴᵍᵥ .* population.nᵢᵍ
+    compartments[:, :, :, :, 6]  .= epi_params.ρᴾᴰᵍᵥ .* population.nᵢᵍ
+    compartments[:, :, :, :, 7]  .= epi_params.ρᴴᴿᵍᵥ .* population.nᵢᵍ
+    compartments[:, :, :, :, 8]  .= epi_params.ρᴴᴰᵍᵥ .* population.nᵢᵍ
+    compartments[:, :, :, :, 9]  .= epi_params.ρᴿᵍᵥ .* population.nᵢᵍ
+    compartments[:, :, :, :, 10] .= epi_params.ρᴰᵍᵥ .* population.nᵢᵍ
+    isfile(output_fname) && rm(output_fname)
+
+    nccreate(output_fname, "data", "G", G_coords, "M", M_coords, "T", T_coords, "V", V_coords, "epi_states", S_coords)
+    ncwrite(compartments, output_fname, "data")
+
+end
+
+function save_simulation_netCDF( epi_params::MMCAcovid19.Epidemic_Params, 
+                                 population::MMCAcovid19.Population_Params,
+                                 output_fname::String;
+                                 G_coords = nothing,
+                                 M_coords = nothing,
+                                 T_coords = nothing,
+                                )
+    G = population.G
+    M = population.M
+    T = epi_params.T
+    S = 10
+    S_coords = ["S", "E", "A", "I", "PH", "PD", "HR", "HD", "R", "D"]
+
+    if isnothing(G_coords)
+        G_coords = collect(1:G)
+    end
+    if isnothing(M_coords)
+        M_coords = collect(1:M)
+    end
+    if isnothing(T_coords)
+        T_coords = collect(1:T) 
+    end
+
+    compartments = zeros(Float64, G, M, T, S);
+
+    # Adding 
+    compartments[:, :, :, 1]  .= epi_params.ρˢᵍ .* population.nᵢᵍ
+    compartments[:, :, :, 2]  .= epi_params.ρᴱᵍ .* population.nᵢᵍ
+    compartments[:, :, :, 3]  .= epi_params.ρᴬᵍ .* population.nᵢᵍ
+    compartments[:, :, :, 4]  .= epi_params.ρᴵᵍ .* population.nᵢᵍ
+    compartments[:, :, :, 5]  .= epi_params.ρᴾᴴᵍ .* population.nᵢᵍ
+    compartments[:, :, :, 6]  .= epi_params.ρᴾᴰᵍ .* population.nᵢᵍ
+    compartments[:, :, :, 7]  .= epi_params.ρᴴᴿᵍ .* population.nᵢᵍ
+    compartments[:, :, :, 8]  .= epi_params.ρᴴᴰᵍ .* population.nᵢᵍ
+    compartments[:, :, :, 9]  .= epi_params.ρᴿᵍ .* population.nᵢᵍ
+    compartments[:, :, :, 10] .= epi_params.ρᴰᵍ .* population.nᵢᵍ
+    isfile(output_fname) && rm(output_fname)
+
+    @info "compartments shape: $(size(compartments))"
+
+    nccreate(output_fname, "data", "G", G_coords, "M", M_coords, "T", T_coords, "epi_states", S_coords)
+    ncwrite(compartments, output_fname, "data")
 end
