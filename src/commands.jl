5cdd52db79aa8baab1f7f0321181156660c6cf2c
Isabel <isabel.martinez1@bsc.es>
2024-10-07 18:19:48 +0200

    indicate varibles types when reading metapop file

--- src/commands.jl
+++ src/commands.jl
@@ -193,7 +193,8 @@ function read_config()
     
     # Loading metapopulation patches info (surface, label, population by age)
     metapop_data_filename = joinpath(data_path, data_dict["metapopulation_data_filename"])
-    metapop_df = CSV.read(metapop_data_filename, DataFrame, types=Dict(:id => String))
+    metapop_df = CSV.read(metapop_data_filename, DataFrame, types= Dict("id"=>String, 
+        "area"=>Float64, "Y"=>Float64, "M"=>Float64, "O"=>Float64, "Total"=>Float64))
     
     # Loading mobility network
     mobility_matrix_filename = joinpath(data_path, data_dict["mobility_matrix_filename"])
